bd4bd77740075d5676c92e3bb128c57b2c9d3add
Anton <anton@adc.sh>
2018-05-18 21:59:19 +0100

    export types

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-export { watch as watchBundles } from './browserify'
+import { watch as w } from './browserify'
+
+/**
+ * @typedef {Object} BrowserifyConfig Import browserify types here
+ *
+ * @typedef {Object} BabelifyConfig Import babelify config here
+ *
+ * @typedef {Object} Config
+ * @property {string} from
+ * @property {string} to
+ * @property {BrowserifyConfig} browserify
+ * @property {BabelifyConfig} babelify
+ *
+ * @typedef {Object.<string, string[]>} AliasMap
+ *
+ * @typedef {Object} RoutesConfig
+ * @property {string} dir Directory from which to initialise routes
+ * @property {{}} methods
+ * @property {Router} router `koa-router` instance
+ * @property {boolean} [defaultImports] true if using ES6
+ * @property {AliasMap} [aliases] a map of aliases
+ */
+
+
+/**
+ * @type {(conf: Config) => void}
+ */
+const watchBundles = w
+
+export { watchBundles }
+
+import wr from  './watch-routes'
+
+
+/**
+ * @type {(routesConf: RoutesConfig) => void}
+ */
+const watchRoutes = wr
+
+export default watchRoutes
